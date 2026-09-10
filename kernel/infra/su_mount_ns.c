c66b2558b5f2321c203ca596ffc5f8aa4d721ffd
vandalsquad187 <vandalsquad187@users.noreply.github.com>
2026-09-10 22:43:35 +0200

    fix: 4.14 su_mount_ns uapi mount.h -> linux/mount.h for <5.0 (fix 34526775798)

--- kernel/infra/su_mount_ns.c
+++ kernel/infra/su_mount_ns.c
@@ -13,7 +13,11 @@
 #include <linux/syscalls.h>
 #include <linux/task_work.h>
 #include <linux/version.h>
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
 #include <uapi/linux/mount.h>
+#else
+#include <linux/mount.h>
+#endif
 
 #include "arch.h"
 #include "klog.h" // IWYU pragma: keep
