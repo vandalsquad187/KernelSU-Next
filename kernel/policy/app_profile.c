fc46d705e9f58a1b80135870ac3219f09eee9393
vandalsquad187 <vandalsquad187@users.noreply.github.com>
2026-09-11 07:42:41 +0200

    fix: app_profile.c include kernel_compat.h for ksu_get_uid_t (fix 34561683615)

--- kernel/policy/app_profile.c
+++ kernel/policy/app_profile.c
@@ -15,6 +15,7 @@
 #include "selinux/selinux.h"
 #include "infra/su_mount_ns.h"
 #include "hook/tp_marker.h"
+#include "../kernel_compat.h"
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0)
 static struct group_info root_groups = { .usage = REFCOUNT_INIT(2) };
